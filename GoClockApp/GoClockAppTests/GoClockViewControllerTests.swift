885ff1c20e2fe6186a2c8e73ce41838434532622
nameky <suchbalance@qq.com>
2022-10-28 21:28:57 +0800

    Add first test for GoClockViewController: test_showsClockTimeOnDisplay

--- GoClockApp/GoClockAppTests/GoClockViewControllerTests.swift
+++ GoClockApp/GoClockAppTests/GoClockViewControllerTests.swift
@@ -8,22 +8,71 @@ import XCTest
 @testable import GoClock
 
 final class GoClockViewController: UIViewController {
+    private var clock: GoClock?
     
+    convenience init(clock: GoClock) {
+        self.init()
+        self.clock = clock
+    }
+    
+    var hostSideView: SideView!
+    var guestSideView: SideView!
+    
+    override func viewDidLoad() {
+        guard let clock = clock else { return }
+        hostSideView = SideView(remainingSeconds: clock.sides[0].remainingSeconds)
+        guestSideView = SideView(remainingSeconds: clock.sides[1].remainingSeconds)
+    }
+}
+
+final class SideView: UIView {
+    var timeLabel: UILabel!
+    
+    convenience init(remainingSeconds: UInt) {
+        self.init()
+        timeLabel = UILabel()
+        timeLabel.text = "\(remainingSeconds)"
+    }
 }
 
 final class GoClockViewControllerTests: XCTestCase {
 
-    func test_showsClockTimeOnDisplay() throws {
-        let _ = makeSUT()
+    func test_showsClockTimeOnDisplay() {
+        let sut = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.hostTime, 2)
+        XCTAssertEqual(sut.guestTime, 2)
     }
     
     // MARK: -- Helpers
     
     private func makeSUT() -> GoClockViewController {
-        GoClockViewController()
+        let side0 = MockSide(remainingSeconds: 2)
+        let side1 = MockSide(remainingSeconds: 2)
+        let clock = MockGoClock(sides: [side0, side1])
+        let sut = GoClockViewController(clock: clock)
+        
+        trackForMemoryLeaks(side0)
+        trackForMemoryLeaks(side1)
+        trackForMemoryLeaks(clock)
+        trackForMemoryLeaks(sut)
+        
+        return sut
     }
     
     private class MockGoClock: GoClock {
         
+    }
+}
+
+extension GoClockViewController {
+    var hostTime: UInt {
+        UInt(hostSideView.timeLabel.text!)!
+    }
+    
+    var guestTime: UInt {
+        UInt(guestSideView.timeLabel.text!)!
     }
 }
