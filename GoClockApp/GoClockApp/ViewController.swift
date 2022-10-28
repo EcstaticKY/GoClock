149fe7069adb8119fb81dffa66a8b241d2503831
nameky <suchbalance@qq.com>
2022-10-28 20:18:09 +0800

    Add GoClockApp and GoClock workspace

---
+++ GoClockApp/GoClockApp/ViewController.swift
@@ -0,0 +1,17 @@
+///
+/// Created by Zheng Kanyan on 2022/10/28.
+/// 
+///
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
