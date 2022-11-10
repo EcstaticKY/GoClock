a644f8a7aa31f71d4d33cc602a5f5cc6d70af1c4
zky <zhengkanyan@gmail.com>
2022-11-10 10:32:00 +0800

    Add more tests about multiple time switching and more time clapsed

--- GoClock/GoClockTests/GoClockIntegrationTests.swift
+++ GoClock/GoClockTests/GoClockIntegrationTests.swift
@@ -7,20 +7,32 @@ import XCTest
 @testable import GoClock
 
 final class GoClockIntegrationTests: XCTestCase {
-    
     func test_GoClockInstanceCallsUpdatedWhenTimerTickingOneSecond() {
         let sut = makeSUT()
 
         let exp = expectation(description: "Wait for clock updated")
-        exp.expectedFulfillmentCount = 2
+        exp.expectedFulfillmentCount = 6
         sut.setUpdatedClosure {
             exp.fulfill()
         }
 
         sut.switchSide()
-        wait(for: [exp], timeout: 1.1)
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.1) {
+            DispatchQueue.main.async {
+                sut.switchSide()
+                DispatchQueue.global().asyncAfter(deadline: .now() + 1.1) {
+                    DispatchQueue.main.async {
+                        sut.switchSide()
+                    }
+                }
+            }
+        }
+        
+        wait(for: [exp], timeout: 3.3)
 
-        XCTAssertEqual((sut.sides[0] as! ConcreteSide).remainingSeconds, 29)
+        XCTAssertEqual((sut.sides[0] as! ConcreteSide).remainingSeconds, 28)
+        XCTAssertEqual((sut.sides[1] as! ConcreteSide).remainingSeconds, 29)
     }
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> GoClock {
