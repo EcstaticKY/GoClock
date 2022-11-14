dee5017f19f48222308e124074d86a048df8e54f
zky <zhengkanyan@gmail.com>
2022-11-14 11:50:55 +0800

    Actions alter states correctly in GoClockEx

--- GoClock/GoClockTests/GoClockExTests.swift
+++ GoClock/GoClockTests/GoClockExTests.swift
@@ -9,26 +9,125 @@ import XCTest
 @testable import GoClock
 
 public class GoClockEx {
-    public enum State {
-        case ready, running, pausing, timedOut
+    public enum State: Equatable {
+        case ready, running(atHost: Bool), pausing(atHost: Bool), timedOut
     }
     
-    public var state = State.ready
+    public enum RemainingTime: Equatable {
+        case free(seconds: UInt)
+        case countDown(seconds: UInt, times: UInt)
+    }
     
-    private var sides: [ConcreteSide]
+    public private(set) var state = State.ready
+    public var hostRemainingTime: RemainingTime
+    public var guestRemainingTime: RemainingTime
     
-    init(sides: [ConcreteSide]) {
-        self.sides = sides
+    private let timeSetting: TimeSetting
+    
+    public init(timeSetting: TimeSetting) {
+        self.timeSetting = timeSetting
+        hostRemainingTime = .free(seconds: timeSetting.freeTimeSeconds)
+        guestRemainingTime = .free(seconds: timeSetting.freeTimeSeconds)
+    }
+    
+    @discardableResult
+    public func start() -> Bool {
+        state = .running(atHost: true)
+        return true
+    }
+    
+    @discardableResult
+    public func switchSide() -> Bool {
+        guard case .running(let atHost) = state else {
+            return false
+        }
+        
+        state = .running(atHost: !atHost)
+        return true
+    }
+    
+    @discardableResult
+    public func pause() -> Bool {
+        guard case .running(let atHost) = state else {
+            return false
+        }
+        
+        state = .pausing(atHost: atHost)
+        return true
+    }
+    
+    @discardableResult
+    public func resume() -> Bool {
+        guard case .pausing(let atHost) = state else {
+            return false
+        }
+        
+        state = .running(atHost: atHost)
+        return true
+    }
+    
+    @discardableResult
+    public func reset() -> Bool {
+        guard case .pausing = state else {
+            return false
+        }
+        
+        state = .ready
+        return true
     }
 }
 
 final class GoClockExTests: XCTestCase {
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_actionsAlterStatesCorrectly() {
+        let sut = makeSUT()
+        
+        // Ready state on create
+        XCTAssertEqual(sut.state, .ready)
+        XCTAssertEqual(sut.hostRemainingTime, .free(seconds: 300))
+        XCTAssertEqual(sut.guestRemainingTime, .free(seconds: 300))
+        
+        // Only start can work on ready state
+        XCTAssertFalse(sut.switchSide())
+        XCTAssertFalse(sut.pause())
+        XCTAssertFalse(sut.resume())
+        XCTAssertFalse(sut.reset())
+        
+        // Start alters state to running at host
+        XCTAssertTrue(sut.start())
+        XCTAssertEqual(sut.state, .running(atHost: true))
+        
+        // Switch side toggles if running state at host side
+        XCTAssertTrue(sut.switchSide())
+        XCTAssertEqual(sut.state, .running(atHost: false))
+        XCTAssertTrue(sut.switchSide())
+        XCTAssertEqual(sut.state, .running(atHost: true))
+        XCTAssertTrue(sut.switchSide())
+        XCTAssertEqual(sut.state, .running(atHost: false))
+        
+        // Pause alters state to pausing
+        XCTAssertTrue(sut.pause())
+        XCTAssertEqual(sut.state, .pausing(atHost: false))
+        XCTAssertFalse(sut.switchSide())
+        
+        // Resume alters state back to running at side before pausing
+        XCTAssertTrue(sut.resume())
+        XCTAssertEqual(sut.state, .running(atHost: false))
+        
+        // Reset works only on pausing state and alters state to ready
+        XCTAssertFalse(sut.reset())
+        sut.pause()
+        XCTAssertTrue(sut.reset())
+        XCTAssertEqual(sut.state, .ready)
+    }
+    
+    // MARK: -- Helpers
+    
+    private func makeSUT() -> GoClockEx {
+        let timeSetting = TimeSetting(freeTimeSeconds: 300, countDownSeconds: 30, countDownTimes: 3)
+        let sut = GoClockEx(timeSetting: timeSetting)
+        
+        trackForMemoryLeaks(sut)
+        return sut
     }
 }
